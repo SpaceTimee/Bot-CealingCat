12996c105ef2c6299531fc21325dca728a9fd0b6
Space Time <Zeus6_6@163.com>
2025-12-19 17:28:23 +0800

    1.0.4 更新

--- Bot-CealingCat-Client/src/App.vue
+++ Bot-CealingCat-Client/src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <main>
     <h1>Bot CealingCat</h1>
+    <p>绝赞建设中，可用指令：</p>
+    <ul>
+      <li>生成伪造规则 <code>/generate [string]domain</code></li>
+      <li>搜索内置规则 <code>/search [string]domain</code></li>
+      <li>测试内置规则 <code>/check</code></li>
+      <li>下载内置规则 <code>/download</code></li>
+      <li>喵~ <code>/meow</code></li>
+    </ul>
+    <p>1.0.4 by Space Time</p>
   </main>
 </template>
 
