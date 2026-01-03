54bfe3120f4d9513c208b7ee65e4638249ffbdaf
Space Time <Zeus6_6@163.com>
2026-01-03 17:55:50 +0800

    1.0.6 更新

--- Bot-CealingCat-Client/vite.config.ts
+++ Bot-CealingCat-Client/vite.config.ts
@@ -1,9 +1,37 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import plugin from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [plugin()],
+  plugins: [
+    plugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Bot CealingCat',
+        short_name: 'Bot CealingCat',
+        description: 'Cealing 猫猫，把你超超',
+        theme_color: '#ea909a',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
+    Sitemap({
+      hostname: 'https://bot.spacetimee.xyz'
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
